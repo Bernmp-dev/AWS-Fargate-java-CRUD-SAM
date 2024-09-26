9859b50c5ad74e1d5dac5775007d8b372594f174
bernmp-dev <bernardomp.dev@gmail.com>
2024-09-26 16:45:08 -0300

    fix: template.yaml

--- src/main/java/com/bernmpdev/productsfargate/config/DynamoDBConfig.java
+++ src/main/java/com/bernmpdev/productsfargate/config/DynamoDBConfig.java
@@ -3,17 +3,17 @@ package com.bernmpdev.productsfargate.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+//import software.amazon.awssdk.auth.credentials.AwsCredentials;
+//import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoDBConfig {
-    Region region = Region.SA_EAST_1;
+    Region region = Region.US_EAST_2;
 
     @Value("${amazon.aws.accesskey}")
     private String amazonAWSAccessKey;
@@ -22,14 +22,14 @@ public class DynamoDBConfig {
     private String amazonAWSSecretKey;
 
 
-    private AwsCredentialsProvider awsCredentialsProvider() {
-        AwsCredentials awsCredentials = AwsBasicCredentials.create(
-                amazonAWSAccessKey,
-                amazonAWSSecretKey
-        );
-
-        return StaticCredentialsProvider.create(awsCredentials);
-    }
+//    private AwsCredentialsProvider awsCredentialsProvider() {
+//        AwsCredentials awsCredentials = AwsBasicCredentials.create(
+//                amazonAWSAccessKey,
+//                amazonAWSSecretKey
+//        );
+//
+//        return StaticCredentialsProvider.create(awsCredentials);
+//    }
 
     private DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
